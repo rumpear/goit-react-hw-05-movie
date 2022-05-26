73379cf194e67a04c522fe1451601e6662ba9dfc
Roman <77742141+rumpear@users.noreply.github.com>
2022-05-26 17:01:49 +0300

    upd hooks

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,15 +1,15 @@
 import { useEffect, useState } from 'react';
 
-export const useFetch = (request, deps) => {
+export const useFetch = (requestFn, deps) => {
   const [state, setState] = useState();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetch = async () => {
+    const asyncRequest = async () => {
       setLoading(true);
       try {
-        const data = await request();
+        const data = await requestFn();
         console.log('useEffect', data);
         setState(data);
       } catch (error) {
@@ -18,7 +18,7 @@ export const useFetch = (request, deps) => {
         setLoading(false);
       }
     };
-    fetch();
+    asyncRequest();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, deps);
   console.log(state, 'after useEffect');
