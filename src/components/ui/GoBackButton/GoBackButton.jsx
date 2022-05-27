2cf005ed55da332e56cf6fb1b8c7df13d91d78cf
Roman <roman.f.grusha@gmail.com>
2022-05-27 22:14:31 +0300

    upd GoBackButton

--- src/components/ui/GoBackButton/GoBackButton.jsx
+++ src/components/ui/GoBackButton/GoBackButton.jsx
@@ -1,4 +1,4 @@
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { Button } from './GoBackButton.styled';
 import { IoReturnDownBack } from 'react-icons/io5';
 import { useEffect, useState } from 'react';
